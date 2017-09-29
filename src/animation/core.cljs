78f7d624a59dcdf41b27e77fb4655fdf1e4543af
Andrej Lamov <andrej.lamov@gmail.com>
2017-09-29 20:03:34 +0200

    Create base template

--- src/animation/core.cljs
+++ src/animation/core.cljs
@@ -6,15 +6,26 @@
 
 (enable-console-print!)
 
-(defn root []
-  [:div.ui.container
-   {:join #(.. % (text "hello"))}
-   ])
+(def state (atom {:left
+                  ["chrome", "firefox", "edge"]
+                  :right
+                  ["opera", "safari"]}))
+
+(defn root [state]
+  [:div.ui.two.column.grid.container
+   {:join #(.. % (style "padding-top" "2em"))}
+
+   [:div.column>:div.ui.list
+    (for [item (:left state)]
+      [:i.huge.icon {:join #(.. % (classed item "true"))}])]
+   [:div.column>:div.ui.list
+    (for [item (:right state)]
+      [:i.huge.icon {:join #(.. % (classed item "true"))}])]])
 
 (defn main []
   (println "***")
   (r/render
    (.. js/d3 (select "#app"))
-   (root)))
+   (root @state)))
 
 (main)
